ad8873370338c5fce47cf74c6cbaf75fe3c903ce
ebbaj <ebba-jonsson@hotmail.com>
2020-11-12 17:05:04 +0100

    Made footer into template tag

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 //Animations for the fade ins 
 const tl = gsap.timeline({ defaults: { ease: "power1.out"} });
 
-tl.fromTo('.left-side', {opacity: 0}, {opacity: 1, duration: 1});
-tl.fromTo('.right-side', {opacity: 0}, {opacity: 2, duration: 1}, "-=1");
-tl.fromTo('#projects-main', {opacity: 0}, {opacity: 1, duration: 1}, "-=1");
+tl.fromTo('.left-side', {opacity: 0}, {opacity: 1, duration: 1.5});
+tl.fromTo('.right-side', {opacity: 0}, {opacity: 2, duration: 1.5}, "-=1.5");
+tl.fromTo('#projects-main', {opacity: 0}, {opacity: 1, duration: 1.5}, "-=1.5");
 
 
 // Animations for the logo
@@ -15,5 +15,29 @@ logo.addEventListener("mouseover", function() {
 });
 
 logo.addEventListener("mouseout", function() {
-   TweenLite.to(".logo", 1, {rotation:"0_ccw"});
+   TweenLite.to(".logo", 1, {rotation:"0_cw"});
 });
+
+
+//Footer template
+/*
+const template = document.getElementById('template');
+const projectsTemplate = document.getElementById('projects-template');
+
+const node = document.importNode(template.content, true);
+document.body.appendChild(node);
+
+let clone = template.content.cloneNode(true);
+projectsTemplate.appendChild(clone);
+document.body.appendChild(document.importNode(projectsTemplate.content, true));
+*/
+
+/*
+const template = document.querySelector('template');
+const node = document.importNode(template.content, true);
+document.body.appendChild(node);
+*/
+
+const template = document.querySelector('template');
+const node = document.importNode(template.content, true);
+document.body.appendChild(node);
